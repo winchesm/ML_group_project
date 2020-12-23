e13b0478ee6234fc438ebf98fd0700b3c0680f58
kkirsch1 <>
2020-12-23 18:15:04 +0000

    add comments

--- Web Scrape/get_links.py
+++ Web Scrape/get_links.py
@@ -7,23 +7,31 @@ topics = ['Technology', 'Entertainment', 'Design', 'Business', 'Science', 'Globa
 links = []
 for topic in topics:
     URL = 'https://www.ted.com/talks?sort=newest&topics%5B%5D=' + topic
+    # sleep to avoid  HTTp status code 429 Too Many Requests
     time.sleep(10)
+    # request topic URL
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
+    # Fetch list of all items labelled with below class,
+    # the last element is the number of the final page
     max_pages = int(soup.find_all(class_='pagination__item pagination__link')[-1].get_text())
+    # Loop through all pages
     for page in range(max_pages):
         page += 1
         URL = 'https://www.ted.com/talks?sort=newest&page=' + str(page) + '&topics%5B%5D=' + topic
         page = requests.get(URL)
         time.sleep(1.5)
         soup = BeautifulSoup(page.content, 'html.parser')
+        # Find all talk link URLs and append to list
         talk_divs = soup.find_all(class_='talk-link')
         for div in talk_divs:
             links.append(div.a.get('href'))
 
+#Delete duplicate links
 links_nodups = set(links)
 print(len(links) != len(links_nodups))
 
+# Save all links to file
 with open('links.txt', 'w') as f:
     for link in links_nodups:
         f.write(link + '\n')
--- Web Scrape/get_transcripts.py
+++ Web Scrape/get_transcripts.py
@@ -2,19 +2,24 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+# Make list of all links saved in get_links.py
 with open('links.txt') as f:
     links = f.read().splitlines()
-skip = True
+
+#skip = True
 with open('data.csv', 'a', encoding='utf-8') as f2:
     f2.write('Link,Tags,Transcript\n')
     for link in links:
+        # This is used to start off on a specific link after internet disconnected
         # if link == '/talks/dave_brain_what_a_planet_needs_to_sustain_life':
         #     skip = False
         # if skip:
         #     continue
         print(link)
-        URL = 'https://www.ted.com/' + link 
+        # Get specific talk url
+        URL = 'https://www.ted.com' + link 
         page = requests.get(URL)
+        # Sleep to avoid error 429
         time.sleep(1)
         while(page.status_code == 429):
             print('Sleeping tags')
@@ -22,9 +27,11 @@ with open('data.csv', 'a', encoding='utf-8') as f2:
             page = requests.get(URL)
         print('Tags ok')
         soup = BeautifulSoup(page.content, 'html.parser')
+        # Save all topic tags
         tags = []
         for tag in soup.find_all('meta', property='og:video:tag'):
             tags.append(tag.get('content'))
+            # Switch over to transcripts tag
         URL = 'https://www.ted.com/' + link + '/transcript'
         page = requests.get(URL)
         time.sleep(1)
@@ -34,8 +41,12 @@ with open('data.csv', 'a', encoding='utf-8') as f2:
             page = requests.get(URL)
         print('Transcript ok')
         soup = BeautifulSoup(page.content, 'html.parser')
+        # All transcripts are wrapped with an element with
+        # class Grid__cell flx-s:1 p-r:4 
         paragraphs = soup.find_all(class_='Grid__cell flx-s:1 p-r:4')
         transcript = ''
+        # Sup up the parts of transcript and add a "" for every "
+        # due to csv field formatting
         for p in paragraphs:
             transcript += ' '.join(p.get_text().split()).replace('"', '""') + ' '
         f2.write(link + ',"' + str(tags) + '","' + transcript + '"\n')
